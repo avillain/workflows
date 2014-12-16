d5f44e80c4d2e92a5fe035112c79acecdfec469a
avillain <villain.adrien@gmail.com>
2014-12-16 09:56:26 +0100

    pombe versions of workflows

---
+++ indels_pombe.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+
+#$ -cwd
+
+#$ -q pf4 
+
+#$ -N pindel_test
+
+#$ -l mem_total=25G
+
+module load samtools
+module load pindel
+module load SHRiMP bwa
+module load PRISM
+module load SOAPindel
+module load snpEff/3.5
+
+####################
+# PRECONFIGURATION #
+####################
+
+#qsub script.sh file.sam reference.fa unionPB1623.vcf snpEff.config PB1623_pindel.vcf.gz PB1623_soap.vcf.gz PB1623_prism.vcf.gz
+
+#### $1 : samfile.sam $2 : reference.fa $3 : compare.vcf.gz $4 : snpeff.config $5 : comparepindel.vcf.gz $6 : comparesoap.vcf.gz compareprism.vcf.gz
+
+#directories
+TMP="./tmp"
+RESULTS="./results"
+
+if [[ ! -s $RESULTS ]]
+     then
+          mkdir $RESULTS
+fi
+
+if [[ ! -s $TMP ]]
+     then
+          mkdir $TMP
+fi
+
+#filenames
+prefix=`basename ${1%.*}`
+bamfile=$TMP"/"$prefix".bam"
+sortedbam=$TMP"/"$prefix"_sorted"
+sortedbamfile=$sortedbam.bam
+bamindex=$sortedbamfile.bai
+reference=$TMP"/"`basename $2`
+refindex=$reference.fai
+compare=$TMP"/"`basename $3`
+
+compindel=$5
+cp $2 $reference
+ln $3 $compare
+
+#insert size
+insertfile=$TMP"/"$prefix"_insertsize.txt"
+
+if [ ! -f $insertfile ]
+then
+    getinsertsize.py $1 > $insertfile
+fi
+
+insertmean=`grep -o -P "span: mean ([0-9]+)" $insertfile | cut -d " " -f 3`
+insertsd=`grep -o -P "span: mean [0-9]+\.[0-9]+, STD=([0-9]+)" $insertfile | cut -d "=" -f 2`
+readslength=`grep -o -P "length: mean [0-9]+" $insertfile | cut -d " " -f 3`
+
+#samtobam, bam sorting, bam indexing, reference indexing
+if [ ! -f $bamfile ]
+then
+    samtools view -bS $1 > $bamfile
+fi
+
+if [ ! -f $sortedbamfile ]
+then
+    samtools sort $bamfile $sortedbam
+fi
+
+if [ ! -f $bamindex ]
+then
+    samtools index $sortedbamfile $bamindex
+fi
+
+if [ ! -f $refindex ]
+then
+    samtools faidx $reference $refindex
+fi
+
+
+#############
+# DETECTION #
+#############
+
+### Pindel
+
+pindelconfigfile=$TMP"/""pindel$prefix.config"
+pindout=$TMP"/"$prefix"_D"
+pindvcf=$TMP"/"$prefix"_pindel.vcf"
+pindvcf_filt=$RESULTS"/"$prefix"_pindel_filtered.vcf"
+pindvcf_filtgz=$pindvcf_filt.gz
+pindelsubtracted=$RESULTS"/"$prefix"_pindel_subtracted.vcf"
+pindvcf0=$RESULTS"/"$prefix"_pindel_filt0.vcf"
+pindvcf25=$RESULTS"/"$prefix"_pindel_filt25.vcf"
+pindvcf50=$RESULTS"/"$prefix"_pindel_filt50.vcf"
+pindvcf100=$RESULTS"/"$prefix"_pindel_filt100.vcf"
+
+#configuration
+echo -e "$sortedbamfile\t$insertmean\t$prefix" > $pindelconfigfile
+
+#detection
+pindel -f $reference -i $pindelconfigfile -c ALL -o $TMP"/"$prefix
+
+#conversion
+pindel2vcf -p $pindout -r $reference -R spombe -d 09052011 -v $pindvcf
+
+#filtering
+grep "#" $pindvcf > $pindvcf_filt
+awk '/^I.*/ {split($10,tab,":"); split(tab[2],ad,","); if(ad[2]>=30 && match(tab[1],/[01]\/1/) && $7=="PASS" && (($1=="III" && ($2>23139 || $2<2440994)) || ($1=="I" && ($2>7618 || $2<5569804)) || ($1=="II" && $2<4532901))) print $0}' $pindvcf >> $pindvcf_filt
+
+#subtraction
+
+bgzip $pindvcf_filt
+tabix -p vcf $pindvcf_filtgz
+
+vcf-isec -f -c $pindvcf_filtgz $5 > $pindelsubtracted
+
+gunzip $pindvcf_filtgz
+rm $pindvcf_filtgz.tbi
+
+# division in subclasses
+grep "#" $pindelsubtracted > $pindvcf0
+awk 'BEGIN {OFS = "\t"} !/^#.*/ {split($10,geno,":");split(geno[2],ad,","); if(ad[2]>=10 && ad[2]*100/(ad[1]+ad[2])<=15) print $0}' $pindelsubtracted >> $pindvcf0
+
+grep "#" $pindelsubtracted > $pindvcf25
+awk 'BEGIN {OFS = "\t"} !/^#.*/ {split($10,geno,":");split(geno[2],ad,","); if(ad[2]>=10 && ad[2]*100/(ad[1]+ad[2])>15 && ad[2]*100/(ad[1]+ad[2])<=35) print $0}' $pindelsubtracted >> $pindvcf25
+
+grep "#" $pindelsubtracted > $pindvcf50
+awk 'BEGIN {OFS = "\t"} !/^#.*/ {split($10,geno,":");split(geno[2],ad,","); if(ad[2]>=10 && ad[2]*100/(ad[1]+ad[2])>35 && ad[2]*100/(ad[1]+ad[2])<=65) print $0}' $pindelsubtracted >> $pindvcf50
+
+grep "#" $pindelsubtracted > $pindvcf100
+awk 'BEGIN {OFS = "\t"} !/^#.*/ {split($10,geno,":");split(geno[2],ad,","); if(ad[2]>=10 && ad[2]*100/(ad[1]+ad[2])>65) print $0}' $pindelsubtracted >> $pindvcf100
+
+
+### SOAPindel
+
+soapvcf=$TMP"/"$prefix"_soap.vcf"
+soapvcf_filt=$RESULTS"/"$prefix"_soap_filtered.vcf"
+soapvcf_filtgz=$soapvcf_filt.gz
+soapsubtracted=$RESULTS"/"$prefix"_soap_subtracted.vcf"
+mappinglist=$TMP"/"$prefix"_mapping.list"
+readslist=$TMP"/"$prefix"_reads.list"
+searchprefix=$prefix
+
+#configuration
+ls /pasteur/projets/NGS-Dyngen/fastq_files/clean_files/*$searchprefix*.fastq > $readslist
+echo -e "$sortedbamfile\t$insertmean\t$insertsd\t$readslength\tPAIR" > $mappinglist
+
+#detection
+indel_detection.ibam.pl $mappinglist $reference $readslist -wd $TMP
+
+#merging chromosomes files
+grep "#" $TMP/result/chr1:*/*indel.vcf > $soapvcf
+grep -hv "#" $TMP/result/*/*indel.vcf >> $soapvcf
+
+#filtering
+grep "##" $soapvcf > $soapvcf_filt
+grep "#CHR" $soapvcf >> $soapvcf_filt
+grep -P "^[I].*PASS.*1/1" $soapvcf | awk '{split($8,tab,";"); split(tab[1],tab2,"="); if (tab2[2]>=30 && (($1=="III" && ($2>23139 || $2<2440994)) || ($1=="I" && ($2>7618 || $2<5569804)) || ($1=="II" && $2<4532901))) print $0}' >> $soapvcf_filt
+
+bgzip $soapvcf_filt
+tabix -p vcf $soapvcf_filtgz
+
+vcf-isec -f -c $soapvcf_filtgz $6 > $soapsubtracted
+
+gunzip $soapvcf_filtgz
+rm $soapvcf_filtgz.tbi
+
+### PRISM (doit être lancé pour chaque chromosome)
+prismvcf=$TMP"/"$prefix"_prism_unfilt.vcf"
+prismvcf_filt=$RESULTS"/"$prefix"_prism_filtered.vcf"
+prismvcf_filtgz=$prismvcf_filt.gz
+prismsubtracted=$RESULTS"/"$prefix"_prism_subtracted.vcf"
+refdir=$reference".DB.SPLIT/*"
+
+#detection
+for i in $refdir; do chrname=`basename ${i%.*}`; run_PRISM.sh -m $insertmean -e $insertsd -r $i -i $1 -I $TMP -O $TMP"/"$chrname; done
+
+#filtering
+for i in $refdir; do chrname=`basename ${i%.*}`; awk '{ if ($6>=5) print $0 }' $TMP"/"$chrname"/split_all.sam_ns_rmmul_cigar_sorted_sv" > $TMP"/"$chrname"/split_all.sam_ns_rmmul_cigar_sorted_sv_supported"; done
+
+#conversion
+for i in $refdir; do chrname=`basename ${i%.*}`; out=$TMP"/"$prefix"_"$chrname"_prism.vcf"; prism2vcf.py $TMP"/"$chrname"/split_all.sam_ns_rmmul_cigar_sorted_sv_supported" $reference $out; done
+cat $TMP"/"*prism.vcf > $prismvcf
+
+echo -e "##fileformat=VCFv4.0\n####fileDate=20140724\n####source=prism\n##INFO=<ID=DP,Number=1,Type=Integer,Description="Total number of reads in haplotype window">\n##INFO=<ID=SVLEN,Number=1,Type=Integer,Description="Difference in length between REF and ALT alleles">\n##INFO=<ID=SVTYPE,Number=1,Type=String,Description="Type of structural variant">\n###ALT=<ID=ALTER,Description="Alter">\n###FILTER=<ID=q10,Description="Quality below 10">\n###FILTER=<ID=hp10,Description="Reference homopolymer length was longer than 10">\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO" > $prismvcf_filt
+
+
+#echo -e "##fileformat=VCFv4.0\n###fileDate=20140724\n###source=prism\n###reference=/pasteur/projets/NGS-Dyngen/large_indels/test/J64-G15/./tmp/pombe_09052011.fasta.DB.SPLIT\n###INFO=<ID=PROGRAM,Number=1,Type=String,Description="Total number of reads in haplotype window">\n###INFO=<ID=SVTYPE,Number=1,Type=String,Description="Total number of reads 2 in haplotype window">\n###INFO=<ID=SVLEN,Number=1,Type=Integer,Description="Left flank length and right flank length">\n###FILTER=<ID=q10,Description="Quality below 10">\n###FILTER=<ID=hp10,Description="Reference homopolymer length was longer than 10">\n#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO" > $prismvcf_filt
+
+awk '/^I.*/ {split($8,geno,";");split(geno[1],ad,"="); if (ad[2]<25 || ($1=="III" && ($2<=23139 || $2>=2440994)) || ($1=="I" && ($2<=7618 || $2>=5569804)) || ($1=="II" && $2>=4532901)); else print $0}' $prismvcf >> $prismvcf_filt
+
+bgzip $prismvcf_filt
+tabix -p vcf $prismvcf_filtgz
+
+vcf-isec -f -c $prismvcf_filtgz $7 > $prismsubtracted
+
+gunzip $prismvcf_filtgz
+rm $prismvcf_filtgz.tbi
+
+
+###############
+# COMPARAISON #
+###############
+
+minus=$RESULTS"/"$prefix"_pindel_soap_prism_minusPB1623.vcf"
+
+joinx vcf-merge -e -s $pindelsubtracted $soapsubtracted $prismsubtracted > $minus
+
+##############
+# ANNOTATION #
+##############
+
+annot=$RESULTS"/"$prefix"_pindel_soap_prism_minusPB1623_annotated.txt"
+echo "snpEff -c $4 -o txt -no-downstream -no-upstream spombe $minus > $annot"
+snpEff -c $4 -o txt -no-downstream -no-upstream spombe $minus > $annot
+
+
---
+++ snps_spombe.sh
@@ -0,0 +1,375 @@
+#!/bin/bash
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script run the SNP and Indels analysis for one genome using fqCleaner for reads pre-processing (optional), bwa mem for reads mapping, GATK2 for variant calling, and snpEff for variant annotation (optional)
+
+OPTIONS:
+   -h      Show this message
+   -i      Input reads (.fastq format)
+   -j	   Input mate reads (.fastq format)
+   -r      Reference genome (.fasta format)
+   -p      Output files prefix
+   -n      No quality control performed
+   -a      Annotation of variants with SnpEff (see -c and -d options)
+   -c	   SnpEff config file (mandatory if -a)
+   -d      SnpEff database ID of the species  (mandatory if -a)
+   -v      Verbose
+
+EXAMPLE:
+./script.sh -i reads1.fq -j reads2.fq -r ref.fasta -p prefix -n -a -c SNPeffconfig1.txt -d spombe
+EOF
+}
+
+INPUT=
+INPUTMATE=
+REFERENCE=
+VERBOSE=
+NOQC=
+ANNOT=
+CONF=
+ID=
+while getopts “hi:j:r:o:p:c:d:vna” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         i)
+             INPUT=$OPTARG
+             ;;
+	 j)
+	     INPUTMATE=$OPTARG
+	     ;;
+         r)
+             REFERENCE=$OPTARG
+             ;;
+         p)
+             PREFIX=$OPTARG
+             ;;
+         c)
+             CONF=$OPTARG
+             ;;
+         d)
+             ID=$OPTARG
+             ;;
+         v)
+             VERBOSE=1
+             ;;
+         n)
+             NOQC=1
+             ;;
+         a)
+             ANNOT=1
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+echo "[info] parsing command-line"
+
+if [[ -z $INPUT ]] || [[ -z $REFERENCE ]]
+then
+     echo "ERROR : Please supply a .fasta reference genome (-r) and .fastq reads to compare (-i)"
+     usage
+     exit 1
+fi
+
+if [[ -z $INPUTMATE ]]
+then
+	echo "[info] no mate reads file supplied, input is single reads"
+fi
+
+if [[ ! -z $ANNOT ]] && ( [[ -z $CONF ]] || [[ -z $ID ]] )
+then
+	echo "ERROR : SnpEff config file (-c) and SnpEff database ID (-d) are mandatory to perform SnpEff annotation (-a)"
+	usage
+	exit 1
+fi
+
+#basenames
+refname=$(basename "$REFERENCE")
+extension="${refname##*.}"
+refname="${refname%.*}"
+readgroup="@RG\tID:group1\tSM:sample1\tPL:illumina\tLB:lib1\PU:unit1"
+
+#prefix of outputs
+if [[ -z $PREFIX ]]
+then
+      dir=$(dirname $INPUT)
+      PREFIX=${name%.*}"vs"$refname 
+      echo "[info] no prefix supplied, output files will begin with $PREFIX"
+else
+      dir=$(dirname $PREFIX)
+      PREFIX=$(basename $PREFIX)
+      echo "[info] output files will begin with $PREFIX"
+fi
+
+TMP=$dir"/tmp"
+RESULTS=$dir"/results"
+echo "[info] working in $dir/tmp - results stored in $dir/results"
+if [[ ! -s $RESULTS ]]
+     then
+          mkdir $RESULTS
+fi
+
+if [[ ! -s $TMP ]]
+     then
+          mkdir $TMP
+fi
+
+outputsam=$TMP"/"$PREFIX".sam"
+fixedsam=$TMP"/"$PREFIX"_fixed.sam"
+outputbam=$TMP"/"$PREFIX".bam"
+dedupbam=$TMP"/dedup_"$PREFIX".bam"
+dictname=$RESULTS"/"$refname".dict"
+metrics=$TMP"/"$refname".metrics"
+realignedbam=$TMP"/realigned_"$PREFIX".bam"
+rawvariants=$TMP"/"$PREFIX"_raw.vcf"
+
+logfile=$TMP"/"$PREFIX".log"
+REFNAME=$(basename $REFERENCE})
+REFNAME="${REFNAME%.*}"
+refgenome=$RESULTS"/"$REFNAME".fa"
+
+if [ ! -f $refgenome ]
+then 
+      cp $REFERENCE $refgenome
+fi
+
+#Versions
+echo "[info] versions : bwa/0.7.5a GenomeAnalysisTK/2.7-2 snpEff/3.5"
+module load bwa/0.7.5a
+module load GenomeAnalysisTK/2.7-2
+module load snpEff/3.5
+
+#Index reference genome
+fai=$TMP"/"$REFERENCE".fai"
+echo "[info] indexing reference genome"
+if [ ! -f $fai ]
+then
+	samtools faidx $refgenome 
+fi
+
+if [ ! -f $RESULTS"/"$REFNAME".bwt" ]
+then
+        bwa index -a is -p $RESULTS"/"$REFNAME $refgenome 
+fi
+
+if [ ! -f $dictname ]
+then
+        CreateSequenceDictionary REFERENCE=$refgenome OUTPUT=$dictname 
+fi
+
+###################
+# Quality control #
+###################
+# fqCleaner must be in the $PATH
+
+if [[ -z $NOQC ]]
+then
+     INQC=$RESULTS"/"$(basename ${INPUT%.*}"_clean.fastq")
+     if [[ -z $INPUTMATE ]]
+     then
+          echo "[info] performing single-end quality control using fqCleaner"
+          echo '[cmd] fqCleaner.sh -f $INPUT -x $INQC -s "QFAD"'
+          fqCleaner.sh -f $INPUT -x $INQC -s "QFAD" -l 80 -q 30 || exit 1
+     else
+	  INQCMATE=$RESULTS"/"$(basename ${INPUTMATE%.*})"_clean.fastq"
+          echo "[info] performing paired-end quality control using fqCleaner"
+          echo "[cmd] fqCleaner.sh -f $INPUT -r $INPUTMATE -x $INQC -y $INQCMATE -s "QFAD" -l 80 -q 30 "
+          fqCleaner.sh -f $INPUT -r $INPUTMATE -x $INQC -y $INQCMATE -s "QFAD" -l 80 -q 30 || exit 1
+     fi
+     INPUT=$INQC
+     INPUTMATE=$INQCMATE
+fi
+
+if [[ ! -z $INPUTMATE ]]
+then
+     echo '[cmd] bwa mem -M -R '$readgroup $RESULTS"/"$REFNAME $INPUT $INPUTMATE' > $outputsam'
+     bwa mem -M -R $readgroup $RESULTS"/"$REFNAME $INPUT $INPUTMATE > $outputsam || exit 1
+else
+     echo '[cmd] bwa mem -M -R '$readgroup $RESULTS"/"$REFNAME $INPUT' > $outputsam'
+     bwa mem -M -R $readgroup $RESULTS"/"$REFNAME $INPUT > $outputsam || exit 1
+fi
+
+#Mapping
+echo "[info] mapping reads on reference genome"
+echo '[cmd] bwa mem -M -R $readgroup $RESULTS"/"$REFNAME $INPUT > $outputsam'
+bwa mem -M -R $readgroup $RESULTS"/"$REFNAME $INPUT > $outputsam 
+
+#Sam to Bam
+echo '[cmd] FixMateInformation INPUT=$outputsam OUTPUT=$fixedsam'
+FixMateInformation INPUT=$outputsam OUTPUT=$fixedsam
+
+#echo '[cmd] SortSam INPUT=$outputsam OUTPUT=$outputbam SORT_ORDER="coordinate"'
+SortSam INPUT=$fixedsam OUTPUT=$outputbam SORT_ORDER="coordinate" 
+echo '[info] get mapping statistics'
+echo "[cmd] samtools flagstat $outputbam"
+samtools flagstat $outputbam
+
+
+##Mark Duplicates
+echo '[cmd] MarkDuplicates INPUT=$outputbam OUTPUT=$dedupbam M=$metrics'
+MarkDuplicates INPUT=$outputbam OUTPUT=$dedupbam M=$metrics 
+
+echo '[cmd] BuildBamIndex INPUT=$dedupbam'
+BuildBamIndex INPUT=$dedupbam 
+
+#Indel realignment
+echo "[info] indels realignment"
+echo '[cmd] GenomeAnalysisTK -T RealignerTargetCreator -R $refgenome -I $dedupbam -o $TMP"/"$PREFIX"_target_intervals.list"'
+GenomeAnalysisTK -T RealignerTargetCreator -R $refgenome -I $dedupbam -o $TMP"/"$PREFIX"_target_intervals.list" 
+
+echo '[cmd] GenomeAnalysisTK -T IndelRealigner -R $refgenome -I $dedupbam -targetIntervals $TMP"/"$PREFIX"_target_intervals.list" -o $realignedbam'
+GenomeAnalysisTK -T IndelRealigner -R $refgenome -I $dedupbam -targetIntervals $TMP"/"$PREFIX"_target_intervals.list" -o $realignedbam 
+
+pileup=$TMP"/"$PREFIX".mpileup"
+cover=$TMP"/"$PREFIX".txt"
+covplot=$TMP"/"$PREFIX"_coverage.jpeg"
+
+#adjust coverage limit for snp filtering
+echo "[info] adjusting coverage limit for snp filtering"
+echo '[cmd] samtools mpileup -f $refgenome $realignedbam > $pileup'
+#echo '[cmd] cov=`awk '{ total += $4;count++ } END {print total/count}' $pileup`'
+#echo '[cmd] res=$(echo "$cov / 2" |bc )'
+samtools mpileup -f $refgenome $realignedbam > $pileup 
+#cov=`awk '{ total += $4;count++ } END {print total/count}' $pileup`
+#res=$(echo "$cov / 2" |bc )
+res=15
+#coverage plot
+#echo "[info] plot mapping coverage along genome"
+#echo "[cmd] cat $pileup | cut -f 2,4 > $cover | awk + gnuplot magic"
+
+#cat $pileup | cut -f 2,4 > $cover
+
+#awk 'BEGIN { OFS = "\t" } ;($1-p2)>1{
+#for(i=p2+1;i<$1;i++)
+#print i,0
+#}
+#{p2=$1}1' $cover | gnuplot -e "set term jpeg ; set output '$covplot' ; set ylabel 'Position on genome' ; set xlabel 'Coverage' ;  set style data linespoints ; set title 'Coverage along reference' ; set key reverse Left outside ; set grid ; plot '<cat' using 1:2 lt 1 lw 2 smooth bezier title '$PREFIX'"
+
+#Calling
+##GATK
+echo '[info] SNPs and Indels calling'
+echo '[cmd] GenomeAnalysisTK -T UnifiedGenotyper -R $refgenome -I $realignedbam -ploidy 1 -glm BOTH -stand_call_conf 10 -stand_emit_conf 5 -o $rawvariants'
+GenomeAnalysisTK -T UnifiedGenotyper -R $refgenome -I $realignedbam -ploidy 1 -glm BOTH -stand_call_conf 10 -stand_emit_conf 5 -o $rawvariants 
+
+###SNPs
+##filenames and filters
+rawsnps=$TMP"/"$PREFIX"_snps_raw.vcf"
+filtexpr="DP<$res || FS > 60.0 || MQ < 40.0"
+filtname="cov"$res
+filteredsnps=$TMP"/"$PREFIX"snps_cov"$res".vcf"
+filteredcovsnps=$TMP"/"$PREFIX"snps_cov"$res"selected.vcf"
+filteredhetall=$TMP"/"$PREFIX"snpsall_filtered.vcf"
+filteredhetsnps=$RESULTS"/"$PREFIX"snps_filtered.vcf"
+
+echo "[info] filtering SNPs using expression $filtexpr"
+echo '[cmd] GenomeAnalysisTK -T SelectVariants -R $refgenome -V $rawvariants -selectType SNP -o $rawsnps' 
+GenomeAnalysisTK -T VariantFiltration -R $refgenome -V $rawsnps --filterExpression "$filtexpr" --filterName $filtname -o $filteredsnps 
+GenomeAnalysisTK -T SelectVariants -R $refgenome --variant $filteredsnps -select "vc.isNotFiltered()" --out $filteredcovsnps
+
+GenomeAnalysisTK -T SelectVariants -R $refgenome -V $rawvariants -selectType SNP -o $rawsnps 
+GenomeAnalysisTK -T VariantFiltration -R $refgenome -V $rawsnps --filterExpression "$filtexpr" --filterName $filtname -o $filteredsnps 
+GenomeAnalysisTK -T SelectVariants -R $refgenome --variant $filteredsnps -select "vc.isNotFiltered()" --out $filteredhetall 
+
+grep "#" $filteredhetall > $filteredhetsnps
+awk '/^I.*/ { if (($1=="III" && ($2<=23139 || $2>=2440994)) || ($1=="I" && ($2<=7618 || $2>=5569804)) || ($1=="II" && $2>=4532901)); else print $0}' $filteredhetall >> $filteredhetsnps
+
+echo '[cmd] grep "#" "$filteredcovsnps" > "$filteredhetsnps"'
+echo "[cmd] grep -v \"#\" $filteredcovsnps | awk '{split($10,geno,":");split(geno[2],ad,\",\"); if(ad[1]*100/(ad[1]+ad[2])<5) print $0}' >> \"$filteredhetsnps\""
+grep '#' "$filteredcovsnps" > "$filteredhetsnps" # header
+grep -v '#' "$filteredcovsnps" | awk '{split($10,geno,":");split(geno[2],ad,","); if(ad[1]*100/(ad[1]+ad[2])<5) print $0}' >> "$filteredhetsnps" # filter on sample/genotype field : 95% of reads must give the same information (e.g. 134 A and 67 T isn't a SNP)
+
+###Indels
+rawindels=$TMP"/"$PREFIX"_indels_raw.vcf"
+filteredindels_tmp=$TMP"/"$PREFIX"indels_filtering.vcf"
+filteredallindels=$TMP"/"$PREFIX"indelsall_filtered.vcf"
+filteredindels=$RESULTS"/"$PREFIX"indels_filtered.vcf"
+filtindel="QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0"
+GenomeAnalysisTK -T SelectVariants -R $refgenome -V $rawvariants -selectType INDEL -o $rawindels 
+GenomeAnalysisTK -T VariantFiltration -R $refgenome -V $rawindels --filterExpression "$filtindel" --filterName "base" -o $filteredindels_tmp 
+GenomeAnalysisTK -T SelectVariants -R $refgenome --variant $filteredindels_tmp -select "vc.isNotFiltered()" --out $filteredallindels
+
+grep "#" $filteredallindels > $filteredindels
+awk '/^I.*/ { if (($1=="III" && ($2<=23139 || $2>=2440994)) || ($1=="I" && ($2<=7618 || $2>=5569804)) || ($1=="II" && $2>=4532901)); else print $0}' $filteredallindels >> $filteredindels
+
+snpminus=$RESULTS"/"$PREFIX"snps_minus_union1623.vcf"
+indelminus=$RESULTS"/"$PREFIX"indels_minus_union1623.vcf"
+
+bgzip $filteredhetsnps
+tabix -p vcf $filteredhetsnps.gz
+vcf-isec -c $filteredhetsnps.gz /pasteur/projets/NGS-Dyngen/snps/PB1623/union/PB1623snps_raw.vcf.gz > $snpminus
+gunzip $filteredhetsnps.gz
+rm $filteredhetsnps.gz.tbi
+
+bgzip $filteredindels
+tabix -p vcf $filteredindels.gz
+vcf-isec -c $filteredindels.gz /pasteur/projets/NGS-Dyngen/snps/PB1623/union/PB1623indels_raw.vcf.gz > $indelminus
+gunzip $filteredindels.gz
+rm $filteredindels.gz.tbi
+
+allvariants=$RESULTS"/"$PREFIX"allvariants_filtered_minus_union1623.vcf"
+tmpvar=$TMP"/allvariants_minus1623_tmp.vcf"
+
+echo "[info] merging snps and indels in the same .vcf file"
+grep -v '#' $snpminus > $tmpvar
+grep -v '#' $indelminus >> $tmpvar
+
+grep '#' $snpminus > $allvariants
+grep -P "^MT\t" $tmpvar | sort -k2,2n >> $allvariants
+grep -P "^AB325691\t" $tmpvar | sort -k2,2n >> $allvariants
+grep -P "^MTR\t" $tmpvar | sort -k2,2n >> $allvariants
+grep -P "^III\t" $tmpvar | sort -k2,2n >> $allvariants
+grep -P "^II\t" $tmpvar | sort -k2,2n >> $allvariants
+grep -P "^I\t" $tmpvar | sort -k2,2n >> $allvariants
+
+#Annotation using snpEff
+if [[ ! -z $ANNOT ]]
+then
+        configfile=$CONF
+        refID=$ID
+        echo "[info] annotation using snpEff : config file $CONF and ref ID $ID"
+        #snps
+        snpeffannotation=$TMP"/"$PREFIX"_effects.vcf"
+        snpeffannotated=$RESULTS"/"$PREFIX"allvariants_annotated_minus_union1623.vcf"
+        snpstats=$TMP"/"$PREFIX"annot"
+        echo "[cmd] snpEff -c $configfile -v -s $snpstats -o gatk $refID $allvariants > $snpeffannotation"
+        snpEff -c $configfile -v -s $snpstats -o gatk $refID $allvariants > $snpeffannotation
+        GenomeAnalysisTK -T VariantAnnotator -R $refgenome -A SnpEff --variant $allvariants --snpEffFile $snpeffannotation -o $snpeffannotated
+fi
+
+#if [[ ! -z $ANNOT ]]
+#then
+#	configfile=$CONF
+#	refID=$ID
+#	echo "[info] annotation using snpEff : config file $CONF and ref ID $ID"
+#	#snps
+#	snpeffannotation=$TMP"/"$PREFIX"snps_effects.vcf"
+#	snpeffannotated=${filteredhetsnps%.*}"_annotated.vcf"
+#	snpstats=$TMP"/"$PREFIX"snpannot"
+#	echo "[cmd] snpEff -c $configfile -v -s $snpstats -o gatk $refID $filteredhetsnps > $snpeffannotation"
+#	snpEff -c $configfile -v -s $snpstats -o gatk $refID $filteredhetsnps > $snpeffannotation
+#	GenomeAnalysisTK -T VariantAnnotator -R $refgenome -A SnpEff --variant $filteredhetsnps --snpEffFile $snpeffannotation  -o $snpeffannotated
+#	#indels
+#	indeleffannotation=$TMP"/"$PREFIX"indels_effects.vcf"
+#	indeleffannotated=${filteredindels%.*}"_annotated.vcf"
+#	indelstats=$TMP"/"$PREFIX"indelsannot"
+#	echo "[cmd] snpEff -c $configfile -v -s $indelstats -o gatk $refID $filteredindels > $indeleffannotation"
+#	snpEff -c $configfile -v -s $indelstats -o gatk $refID $filteredindels > $indeleffannotation
+#	GenomeAnalysisTK -T VariantAnnotator -R $refgenome -A SnpEff --variant $filteredindels --snpEffFile $indeleffannotation  -o $indeleffannotated
+#	
+#	allvariants=$RESULTS"/"$PREFIX"allvariants_annotatedi_minus_union1623.vcf"
+#	echo "[info] merging snps and indels in the same .vcf file"
+#	cat $snpeffannotated > $allvariants
+#	grep -v '#' $indeleffannotated >> $allvariants
+#fi
+
